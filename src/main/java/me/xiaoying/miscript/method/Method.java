b47a29df8abb31664a61be23a88070a64db45198
Hanhan <764932129@qq.com>
2024-05-19 17:24:16 +0800

    feat: make note for Method

--- src/main/java/me/xiaoying/miscript/method/Method.java
+++ src/main/java/me/xiaoying/miscript/method/Method.java
@@ -1,12 +1,14 @@
 package me.xiaoying.miscript.method;
 
 import me.xiaoying.miscript.Blank;
+import me.xiaoying.miscript.Object;
 
 public class Method {
     private final String name;
     private final Blank blank;
     private MethodType methodType = MethodType.PRIVATE;
     private boolean isStatic = false;
+    private Object returnType = null;
 
     public Method(int index, String name, Blank blank) {
         this.name = name;
@@ -17,19 +19,51 @@ public class Method {
         this.blank.clone().run();
     }
 
+    /**
+     * 设置方法类型
+     *
+     * @param type MethodType
+     */
     public void setMethodType(MethodType type) {
         this.methodType = type;
     }
 
+    /**
+     * 获取方法类型<br>
+     * {@code public} -> 公有方法<br>
+     * {@code private} -> 私有方法<br>
+     * {@code protected} -> 限制访问权限<br>
+     *
+     * @return MethodType
+     */
     public MethodType getMethodType() {
         return this.methodType;
     }
 
+    /**
+     * 获取方法是否为静态
+     *
+     * @return Boolean
+     */
     public boolean isStatic() {
         return this.isStatic;
     }
 
+    /**
+     * 设置方法是否为静态
+     *
+     * @param value Boolean
+     */
     public void setStatic(boolean value) {
         this.isStatic = value;
+    }
+
+    /**
+     * 获取返回数据类型
+     *
+     * @return Object
+     */
+    public Object getReturnType() {
+        return this.returnType;
     }
 }
